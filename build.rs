fa0198d0df4e5beae14f5beac2a8ef712bbf8664
Caleb Smith <caleb@myrvmail.com>
2022-07-25 08:35:48 -0500

    Fix non-mold linking

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 fn main() {
     cxx_build::bridge("src/inner.rs")
-        .file("src/shim.cc")
         .define("RYML_SINGLE_HDR_DEFINE_NOW", None)
         .define("C4CORE_SINGLE_HDR_DEFINE_NOW", None)
-        .compiler("clang++")
         .compile("ryml");
     println!("cargo:rerun-if-changed=src/inner.rs");
+    println!("cargo:rerun-if-changed=src/shim.cc");
+    println!("cargo:rerun-if-changed=include/shim.h");
     println!("cargo:rustc-link-lib=ryml");
 }
