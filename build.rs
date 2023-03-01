8e6c7d809a218c396e2da415aad3c107a15561f9
Muneeb Ahmed <126654084+neebyA@users.noreply.github.com>
2023-02-28 23:43:49 -0800

    Add macOS compiler compatibility

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     cxx_build::bridge("src/inner.rs")
         .define("RYML_SINGLE_HDR_DEFINE_NOW", None)
         .define("C4CORE_SINGLE_HDR_DEFINE_NOW", None)
+        .flag_if_supported("-std=c++17")
         .compile("ryml");
     println!("cargo:rerun-if-changed=src/inner.rs");
     println!("cargo:rerun-if-changed=src/shim.cc");
